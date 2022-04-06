36931cb83d971c0d42e104d00af0b51a3f6222be
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-04-06 19:36:46 -0300

    Exercicios de fixacao 01 a 05 - B4D1

---
+++ fundamentos/b04-intro-javascript-e-logica/d01-js-intro-e-primeiros-passos/fixacao-01/scripts.js
@@ -0,0 +1,13 @@
+const myName = "João";
+const birthCity = "Capinzal";
+let birthYear = 1996;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Concórdia";
+
+console.log(birthCity);
