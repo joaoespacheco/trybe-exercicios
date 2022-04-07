c07c2397772a78c316b4e865fa862a095be92464
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-04-07 16:24:23 -0300

    Conclusao de exercicios de fixação - B4D2

---
+++ fundamentos/b04-intro-javascript-e-logica/d02_javascript-array-e-loop-for/fixacao_02/scrypt_01.js
@@ -0,0 +1,21 @@
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
+
+
+
+// OUTROS EXEMPLOS:
+
+// let cars = ["Saab", "Volvo", "BMW"];
+
+// for (let index = 0; index < cars.length; index += 1) {
+//   console.log(cars[index]);
+// }
+
+// let numero = 7;
+// for (let contador = 1; contador < 9; contador = contador + 1) {
+// console.log("");
+// console.log(numero * contador);
+// }
---
+++ fundamentos/b04-intro-javascript-e-logica/d02_javascript-array-e-loop-for/fixacao_03/scrypt_01.js
@@ -0,0 +1,4 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let varPrint of names) {
+    console.log(varPrint);
+}
