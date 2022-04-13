f2acb740a82efebc8e0905d06986c3025fe61603
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-04-13 17:55:36 -0300

    Modificação em exercicio 01 - bonus - B04d03

--- fundamentos/b04-intro-javascript-e-logica/d03-intro-js-e-logica/exercicios-01/scrypt_Bonus.js
+++ fundamentos/b04-intro-javascript-e-logica/d03-intro-js-e-logica/exercicios-01/scrypt_Bonus.js
@@ -4,16 +4,16 @@
 // imprima na tela um quadrado feito de asteriscos de lado 
 // de tamanho n.
 
-let n = 5;
-let asterisco = "";
+// let n = 5;
+// let asterisco = "";
 
-for (let index = 0; index <= n; index += 1) {
-for (let index = 0; index < n; index += 1) {
-  if (asterisco.length !== n)
-    asterisco = asterisco + "*";
-}
-console.log(asterisco);
-}
+// for (let index = 0; index <= n; index += 1) {
+// for (let index = 0; index < n; index += 1) {
+//   if (asterisco.length !== n)
+//     asterisco = asterisco + "*";
+// }
+// console.log(asterisco);
+// }
 
 // ----------EXERCICIO-02----------
 // Para o segundo exercício, faça o mesmo que antes, mas que 
@@ -28,4 +28,24 @@ console.log(asterisco);
 // }
 
 // ----------EXERCICIO-03----------
+
+// let n = 5;
+// let asterisco = "";
+
+// for (let index = n-1; index >= 0; index -= 1) {
+//   asterisco = "";
+//   for (let index2 = 0; index2 < n; index2 += 1) {
+//     if (asterisco.length >= index) {
+//     asterisco = asterisco + "*";
+//     } else {
+//       asterisco = asterisco + " ";
+//     }
+//   }
+//   console.log(asterisco);
+
+// }
+
+
+// ----------EXERCICIO-06----------
+
 
