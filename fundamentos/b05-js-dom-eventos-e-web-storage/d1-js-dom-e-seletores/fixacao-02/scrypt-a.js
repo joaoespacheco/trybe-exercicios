acb6e4cd3ab43dc5f1728026f42e82bba5352bc2
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-04-13 20:21:55 -0300

    Conclusão de exercicio e exemplo-fixação - B5D1

---
+++ fundamentos/b05-js-dom-eventos-e-web-storage/d1-js-dom-e-seletores/fixacao-02/scrypt-a.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = '#AE73F4';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = '#AE73F4';
+
+document.querySelector('h1').style.backgroundColor = '#59B169';
+
+document.querySelector('.emergency-tasks').style.backgroundColor = '#F59D83';
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = '#F9DB5E';
+
+document.querySelector('#footer-container').style.backgroundColor = '#123533';
